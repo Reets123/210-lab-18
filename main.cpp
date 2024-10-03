215ca023ae7ebe5b310bc4b006dd06163b623472
Reets123 <ibrahimalatig@gmail.com>
2024-10-03 09:45:38 +0000

    commit 1

--- main.cpp
+++ main.cpp
@@ -6,4 +6,16 @@
 
 using namespace std;
 
-struct ReviewNo
+struct ReviewNode {
+    double rating;
+    string comment;
+    reviewNode* next;
+};
+
+public:
+    ReviewList() : head(nullptr), tail(nullptr), count(0) []
+
+    void addAtHead(double rating, const string& comment) {
+        ReviewNode* newNode = new ReviewNode{rating, comment, head};
+        head = newN
+    }
