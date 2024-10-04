1190be1962a4f1d87b0991f15db0ecbf7dd6d3ff
Reets123 <ibrahimalatig@gmail.com>
2024-10-04 04:02:55 +0000

    commit 8

--- main.cpp
+++ main.cpp
@@ -9,31 +9,37 @@ using namespace std;
 struct ReviewNode {
     double rating;
     string comment;
-    reviewNode* next;
+    ReviewNode* next;
 };
 
+class ReviewList {
+private:
+    ReviewNode* head;
+    ReviewNode* tail;
+    int count;
+
 public:
-    ReviewList() : head(nullptr), tail(nullptr), count(0) []
+    ReviewList() : head(nullptr), tail(nullptr), count(0) {}
 
     void addAtHead(double rating, const string& comment) {
         ReviewNode* newNode = new ReviewNode{rating, comment, head};
         head = newNode;
         if (tail == nullptr) {
-            tail == newNode
+            tail == newNode;
         }
         count++;
     }
 
-    void addAtTail(double rating, const string& comment){
+    void addAtTail(double rating, const string& comment) {
         ReviewNode* newNode = new ReviewNode{rating, comment, nullptr};
         if (tail == nullptr) {
             head = tail = newNode;
-            else{
-                tail->next = newNode;
-                tail = newNode;
-            }
-            count++;
+        } else {
+            tail->next = newNode;
+            tail = newNode;
         }
+        count++;
+        
     }
 
     void outputReviews() {
